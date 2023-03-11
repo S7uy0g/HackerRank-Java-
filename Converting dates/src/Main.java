fe0e95bf7aa2cef838d591ef545c93f52e7841a9
S7uy0g <RaiJoker103@gmail.com>
2023-03-11 13:38:36 +0545

    getCurrencyInstance(new Locale)

--- Converting dates/src/Main.java
+++ Converting dates/src/Main.java
@@ -9,7 +9,29 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
-public class Main {
+
+class Main {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar date = Calendar.getInstance();
+        date.set(year, month-1, day);//in Java's Calendar class, the month value is zero-indexed
+        DateFormat f = new SimpleDateFormat("EEEE");//format is set to "EEEE", which represents the full day name in the week, like "Monday" or "Tuesday".
+        return f.format(date.getTime()).toUpperCase();
+    }
+
+}
+
+public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -21,7 +43,8 @@ public class Main {
         int day = Integer.parseInt(firstMultipleInput[1]);
 
         int year = Integer.parseInt(firstMultipleInput[2]);
-        String res = Main.findDay(month, day, year);
+
+        String res = Result.findDay(month, day, year);
 
         bufferedWriter.write(res);
         bufferedWriter.newLine();
