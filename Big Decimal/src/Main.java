4bf33c71917ef8f1720d2d2987f0d88c7737e5a1
S7uy0g <RaiJoker103@gmail.com>
2023-03-17 13:01:47 +0545

    BigDecimal IDK

--- Big Decimal/src/Main.java
+++ Big Decimal/src/Main.java
@@ -1,14 +1,14 @@
 import java.math.BigDecimal;
 import java.util.*;
-class Solution{
+class Main {
 
-    public static void main(String []args){
+    public static void main(String[] args) {
         //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
         }
         sc.close();
 
@@ -22,10 +22,8 @@ class Solution{
             }
         });
         //Output
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             System.out.println(s[i]);
         }
     }
-
 }
